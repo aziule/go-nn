f958afda43b6b1f1400b9eb2025e5e022d21a7e2
Will <willyboy_43@hotmail.com>
2017-04-12 14:24:55 +0100

    Fix sigmoid method

--- activation_function.go
+++ activation_function.go
@@ -6,6 +6,6 @@ type ActivationFunction func(float64) float64
 
 func sigmoid() func(float64) float64 {
 	return func(z float64) float64 {
-		return (1 / 1 + math.Exp(-z))
+		return (1 / (1 + math.Exp(-z)))
 	}
 }
