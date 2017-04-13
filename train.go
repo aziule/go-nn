627abfed75bbaa931d35380e5b973ce6c2058ea4
Will <willyboy_43@hotmail.com>
2017-04-13 14:47:23 +0100

    Calculate the error before backpropagation

--- train.go
+++ train.go
@@ -1,6 +1,9 @@
 package go_nn
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 // We assume that the last value of set is the expected output
 // Format: [[[input1.1, input1.2], [expected1]], [[input2.1, input2.2], [expected2]]], and so on...
@@ -13,4 +16,13 @@ func (nn *NeuralNetwork) Train(set [][][]float64) {
 
 func (nn *NeuralNetwork) backPropagation(expected []float64) {
 	fmt.Printf("Expected: %v \n", expected)
+	var errorsSum float64
+
+	for i, n := range nn.OutputLayer.Neurons {
+		errorsSum += calculateError(expected[i], n.Out)
+	}
+}
+
+func calculateError(expected, actual float64) float64 {
+	return 0.5 * math.Pow(expected - actual, 2)
 }
